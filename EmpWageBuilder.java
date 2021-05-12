867c9a2d2056e0e91c3389b22d857b7f2044807b
patelashok <ashok.patel2917@gmail.com>
2021-05-12 15:36:11 +0530

    ADDED: Employee wage using Use case statement

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,25 +1,27 @@
-public class EmpWageBuilder{
+public class EmpWageBuilder {
 	//CONSTANTS 
 	public static final int EMPLOYEE_WAGE_PER_HOUR=20;
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME=2;
 
-	public static void main (String[] args){
+	public static void main (String[] args) {
 		//VARIABLES
 		int empHrs=0;
 		int empWage=0;
 
 		//COMPUTATION
-		double empCheck=Math.floor(Math.random() *10)% 3;
-		if ( empCheck == IS_FULL_TIME ){
-			empHrs=8;
-		}
-		else if(empCheck == IS_PART_TIME){
-			empHrs=4;
-		}
-		else
-			empHrs=0;
-		empWage=empHrs * EMPLOYEE_WAGE_PER_HOUR;
-		System.out.println("Employee Wage: "+empWage);
+		int empCheck=((int)Math.floor(Math.random() *10))% 2;
+		switch(empCheck){
+			case IS_FULL_TIME:
+				empHrs=8;
+				break;
+			case IS_PART_TIME:
+				empHrs=4;
+				break;
+			default:
+				empHrs=0;
+			}
+			empWage=empHrs * EMPLOYEE_WAGE_PER_HOUR;
+			System.out.println("Employee Wage: "+empWage);
 		}
 }
