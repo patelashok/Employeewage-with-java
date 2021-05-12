6f3a1a4b616f39d4205a9e6001cdd23ef817bbf0
patelashok <ashok.patel2917@gmail.com>
2021-05-12 16:02:26 +0530

    ADDED calculating wages max hrs 100 max 20

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,16 +3,22 @@ public class EmpWageBuilder{
 	public static final int EMPLOYEE_WAGE_PER_HOUR=20;
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME=2;
-	public static final int NUMBER_OF_WORKING_HOUR=20;
+	public static final int NUMBER_OF_WORKING_DAYS=20;
+	public static final int MAX_WORKING_HOUR=100;
+	public static final int MAX_WORKING_DAYS=20;
 
 	public static void main (String[] args){
 		//VARIABLES
 		int empHrs=0;
 		int empWage=0;
+		int totalEmpWage=0;
+		int totalWorkingDays=0;
+		int totalEmpHrs=0;
 
 		//COMPUTATION
-		for(int day=1;day<=NUMBER_OF_WORKING_HOUR;day++){
-			int empCheck=((int)Math.floor(Math.random() *10))% 2;
+		while(totalEmpHrs<MAX_WORKING_HOUR && totalWorkingDays<MAX_WORKING_DAYS){
+			totalWorkingDays++;	
+			int empCheck=((int)Math.floor(Math.random() *10))% 3;
 		
 			switch(empCheck){
 				case IS_FULL_TIME:
@@ -24,8 +30,10 @@ public class EmpWageBuilder{
 				default:
 					empHrs=0;
 			}
-			empWage=empHrs * EMPLOYEE_WAGE_PER_HOUR;
+				totalEmpHrs += empHrs;
+				empWage=empHrs * EMPLOYEE_WAGE_PER_HOUR;
+				totalEmpWage += empWage;
 		}
-		System.out.println("Employee Wage Month: "+empWage);
+		System.out.println("Employee Wage Month: "+totalEmpWage);
 		}
 }
