6d7e3fe8ddb0ad056db6a424d92e9bfe62315774
patelashok <ashok.patel2917@gmail.com>
2021-05-12 15:42:53 +0530

    Calculate EMp Wage month

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,27 +1,31 @@
-public class EmpWageBuilder {
+public class EmpWageBuilder{
 	//CONSTANTS 
 	public static final int EMPLOYEE_WAGE_PER_HOUR=20;
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME=2;
+	public static final int NUMBER_OF_WORKING_HOUR=20;
 
-	public static void main (String[] args) {
+	public static void main (String[] args){
 		//VARIABLES
 		int empHrs=0;
 		int empWage=0;
 
 		//COMPUTATION
-		int empCheck=((int)Math.floor(Math.random() *10))% 2;
-		switch(empCheck){
-			case IS_FULL_TIME:
-				empHrs=8;
-				break;
-			case IS_PART_TIME:
-				empHrs=4;
-				break;
-			default:
-				empHrs=0;
+		for(int day=1;day<=NUMBER_OF_WORKING_HOUR;day++){
+			int empCheck=((int)Math.floor(Math.random() *10))% 2;
+		
+			switch(empCheck){
+				case IS_FULL_TIME:
+					empHrs=8;
+					break;
+				case IS_PART_TIME:
+					empHrs=4;
+					break;
+				default:
+					empHrs=0;
 			}
 			empWage=empHrs * EMPLOYEE_WAGE_PER_HOUR;
-			System.out.println("Employee Wage: "+empWage);
+		}
+		System.out.println("Employee Wage Month: "+empWage);
 		}
 }
